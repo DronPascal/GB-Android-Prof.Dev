d80af8f2e99f3a8dd850f409789bdc4b6a0fc42d
Andrew <shpuntik0902@gmail.com>
2021-11-10 23:19:39 +0300

    Updated image loader

--- DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/image/IImageLoader.kt
+++ DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/image/IImageLoader.kt
@@ -1,9 +1,27 @@
 package com.rhinemann.divinecatsource.presentation.image
 
+import android.graphics.drawable.Drawable
+import androidx.annotation.IntRange
+
 /**
  * Created by dronpascal on 29.10.2021.
  */
 interface IImageLoader<T> {
 
-    fun loadTo(url: String, target: T)
+    fun loadTo(
+        url: String,
+        target: T,
+        @IntRange(from = -1) height: Int = -1,
+        @IntRange(from = -1) width: Int = -1,
+        onLoaded: () -> Unit = {}
+    )
+
+    fun loadDrawable(
+        url: String,
+        circleShape: Boolean = false,
+        cornerRadiusPx: Int = 0,
+        @IntRange(from = -1) height: Int = -1,
+        @IntRange(from = -1) width: Int = -1,
+        onLoaded: (Drawable) -> Unit
+    )
 }
