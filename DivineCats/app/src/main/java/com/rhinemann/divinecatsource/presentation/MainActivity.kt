404a4c38c66df5e9ae348c6a6a159213feaa3847
Andrew <shpuntik0902@gmail.com>
2021-11-10 23:34:13 +0300

    Updated main activity presentation

--- DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/MainActivity.kt
+++ DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.rhinemann.divinecatsource.presentation
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.rhinemann.divinecatsource.R
-import com.rhinemann.divinecatsource.core.wrappers.LoadingState
 import com.rhinemann.divinecatsource.databinding.MainActivityBinding
-import com.rhinemann.divinecatsource.presentation.image.IImageLoader
-import org.koin.android.ext.android.inject
+import com.rhinemann.divinecatsource.presentation.navigation.BackButtonListener
+import com.rhinemann.divinecatsource.presentation.navigation.INavigator
+import com.rhinemann.divinecatsource.presentation.navigation.RandomCatScreen
+import com.rhinemann.divinecatsource.presentation.navigation.SearchScreen
+import com.rhinemann.divinecatsource.presentation.screen.randomcat.RandcatFragment
+import com.rhinemann.divinecatsource.presentation.screen.search.SearchFragment
+import org.koin.android.ext.android.getKoin
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 
 /**
@@ -17,34 +20,61 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  */
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
-    private val imageLoader: IImageLoader<ImageView> by inject()
-
     private val viewModel: MainViewModel by viewModel()
 
     private lateinit var binding: MainActivityBinding
 
+    private val scope = getKoin().getOrCreateScope<MainActivity>("MainActivity")
+
+    val navigator: INavigator by scope.inject<INavigator> { parametersOf(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initView()
-        viewModel.viewState.observe(this, ::handleViewState)
-    }
 
-    private fun initView() {
-        val listener = View.OnClickListener { viewModel.getCat() }
-        binding.btnDivine.setOnClickListener(listener)
-        binding.btnBase.setOnClickListener(listener)
-    }
-
-    private fun handleViewState(state: MainViewState?) {
-        state?.cat?.let { imageLoader.loadTo(it.url, binding.imgCat) }
-        when (state?.loadingState) {
-            LoadingState.Loading -> binding.loader.visibility = View.VISIBLE
-            else -> binding.loader.visibility = View.GONE
+        if (savedInstanceState == null) {
+            navigator.navigateTo(
+                screen = RandomCatScreen()
+            )
         }
     }
 
+    private fun initView() {
+        with(binding.bottomNavigationView) {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.miHome -> navigator.backTo(RandomCatScreen.TAG)
+                    R.id.miSearch -> when (navigator.topFragment) {
+                        is SearchFragment -> Unit
+                        else -> {
+                            navigator.backTo(RandomCatScreen.TAG)
+                            navigator.navigateTo(SearchScreen())
+                        }
+                    }
+                    else -> TODO()
+                }
+                true
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        when (val fragment = navigator.topFragment) {
+            // if fragment is home -> close app
+            is RandcatFragment -> moveTaskToBack(true)
+            is BackButtonListener -> fragment.onBackPressed()
+            else -> {
+                navigator.backTo(RandomCatScreen.TAG)
+                binding.bottomNavigationView.selectedItemId = R.id.miHome
+            }
+        }
+        supportFragmentManager.backStackEntryCount.also { println("COUNT = $it") }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.close()
+    }
 }
