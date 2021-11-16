7a21eb7c2de41e15c459373b91c2dffc5a70f573
Andrew <shpuntik0902@gmail.com>
2021-11-16 17:57:36 +0300

    Updated presentation logic

--- DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/screen/search_result/SearchResultFragment.kt
+++ DivineCats/app/src/main/java/com/rhinemann/divinecatsource/presentation/screen/search_result/SearchResultFragment.kt
@@ -2,17 +2,23 @@ package com.rhinemann.divinecatsource.presentation.screen.search_result
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rhinemann.divinecatsource.R
 import com.rhinemann.divinecatsource.core.util.toGone
 import com.rhinemann.divinecatsource.core.util.toVisible
 import com.rhinemann.divinecatsource.core.wrappers.LoadingState
 import com.rhinemann.divinecatsource.databinding.FragmentSearchResultCatsBinding
+import com.rhinemann.divinecatsource.presentation.image.IImageLoader
 import com.rhinemann.divinecatsource.presentation.navigation.BackButtonListener
 import com.rhinemann.divinecatsource.presentation.navigation.INavigator
 import com.rhinemann.divinecatsource.presentation.navigation.SearchScreen
+import com.rhinemann.divinecatsource.presentation.screen.search_result.adapter.CatsAdapter
+import kotlinx.coroutines.flow.*
 import org.koin.android.ext.android.getKoin
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -25,50 +31,53 @@ class SearchResultFragment : Fragment(R.layout.fragment_search_result_cats), Bac
 
     private val viewModel: SearchResultViewModel by viewModel()
 
+    private val imageLoader: IImageLoader<ImageView> by inject()
+
     private val activityScope = getKoin().getScope("MainActivity")
     private val navigator: INavigator by activityScope.inject { parametersOf(requireActivity()) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.state.observe(viewLifecycleOwner, ::handleState)
+        viewModel.loadingState.observe(viewLifecycleOwner, ::handleLoadingState)
         viewModel.events.observe(viewLifecycleOwner, ::handleEvent)
         initView()
     }
 
     private fun initView() {
-
+        initList()
+        setupListeners()
     }
 
-    private fun handleState(state: SearchResultState) =
-        when (state) {
-            is SearchResultState.Loading -> handleLoading(state)
-            is SearchResultState.Result -> handleResult(state)
-            is SearchResultState.Response -> handleResponse(state)
-        }
+    private fun setupListeners() {
+    }
 
-    private fun handleLoading(state: SearchResultState.Loading) =
-        when (state.loadingState) {
+    private fun handleLoadingState(loadingState: LoadingState?) =
+        when (loadingState) {
             LoadingState.Loading -> binding.loader.toVisible()
             else -> binding.loader.toGone()
         }
 
-    private fun handleResult(state: SearchResultState.Result) =
-        when (state) {
-            else -> Unit
-        }
-
-    private fun handleResponse(state: SearchResultState.Response) =
-        when (val component = state.uiComponent) {
-            else -> Unit
-        }
-
     private fun handleEvent(event: SearchResultEvent) =
         when (event) {
-            else -> Unit
+            is SearchResultEvent.OpenScreen.CatFullscreen -> {
+
+            }
         }
+
+    private fun initList() {
+        val catAdapter = CatsAdapter(imageLoader)
+        binding.rvCats.apply {
+            adapter = catAdapter
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            viewModel.searchCats().collectLatest { pagingData ->
+                catAdapter.submitData(pagingData)
+            }
+        }
+    }
 
     override fun onBackPressed() {
         navigator.backTo(SearchScreen.TAG)
     }
-
 }
