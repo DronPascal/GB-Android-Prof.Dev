2e84b19385a7d4da3071ecc228a7f81034c8d1b3
Andrew <shpuntik0902@gmail.com>
2021-12-12 16:37:29 +0300

    Task1. MainActivity espesso test

--- TestsLesson4/app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ TestsLesson4/app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -1,15 +1,19 @@
 package com.geekbrains.tests
 
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
+import junit.framework.TestCase
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -24,6 +28,75 @@ class MainActivityEspressoTest {
     @Before
     fun setup() {
         scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test
+    fun searchEditText_NotNull() {
+        scenario.onActivity {
+            val searchEditText = it.findViewById<TextView>(R.id.searchEditText)
+            TestCase.assertNotNull(searchEditText)
+        }
+    }
+
+    @Test
+    fun toDetailsButton_NotNull() {
+        scenario.onActivity {
+            val toDetailsButton = it.findViewById<TextView>(R.id.toDetailsActivityButton)
+            TestCase.assertNotNull(toDetailsButton)
+        }
+    }
+
+    @Test
+    fun totalCountTextView_NotNull() {
+        scenario.onActivity {
+            val totalCountTextView = it.findViewById<TextView>(R.id.totalCountTextView)
+            TestCase.assertNotNull(totalCountTextView)
+        }
+    }
+
+    @Test
+    fun searchEditText_HasHint() {
+        val assertion: ViewAssertion = matches(withHint(R.string.search_hint))
+        onView(withId(R.id.searchEditText)).check(assertion)
+    }
+
+    @Test
+    fun toDetailsButton_HasText() {
+        val assertion: ViewAssertion = matches(withText(R.string.to_details))
+        onView(withId(R.id.toDetailsActivityButton)).check(assertion)
+    }
+
+    @Test
+    fun totalCountTextView_HasText() {
+        val assertion: ViewAssertion = matches(withText(R.string.results_count))
+        onView(withId(R.id.totalCountTextView)).check(assertion)
+    }
+
+    @Test
+    fun searchEditText_IsCompletelyDisplayed() {
+        onView(withId(R.id.searchEditText)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun toDetailsButton_IsCompletelyDisplayed() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun totalCountTextView_AreInvisible() {
+        onView(withId(R.id.totalCountTextView)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
     }
 
     @Test
